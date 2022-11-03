d952b53d2344ad411adafe6afe11f28acb9750a0
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-11-03 11:02:53 -0400

    vetted all items and ready to test

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
+// Challenge Template: 18-NoSQL/01-Activities/28-Stu_Mini-Project
+
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,6 +13,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
